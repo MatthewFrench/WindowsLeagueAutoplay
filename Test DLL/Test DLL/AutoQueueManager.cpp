08fd1ce7d996094fada48426a2f9b8d00a581407
MatthewFrench <matthewf@ksu.edu>
2016-09-05 03:42:24 -0500

    Fixed comment issues

--- Test DLL/Test DLL/AutoQueueManager.cpp
+++ Test DLL/Test DLL/AutoQueueManager.cpp
@@ -79,14 +79,6 @@ scanReportedLastTime = mach_absolute_time();
 void AutoQueueManager::processLogic() {
 
 
-/*
-currentAwakeTime = mach_absolute_time();
-currentSleepTime  = mach_absolute_time();
-currentPlayTime  = mach_absolute_time();
-currentBreakTime  = mach_absolute_time();
-busySleeping = false;
-busyTakingBreak = false;
-*/
 if (takeBreaks) {
 	if (!busySleeping) {
 		if (getTimeInMilliseconds(mach_absolute_time() - currentAwakeTime) >= awakeTime) {
@@ -225,13 +217,6 @@ if (getTimeInMilliseconds(mach_absolute_time() - actionClick) >= 1000) {
 
 
 		//Click Blind Pick mode
-		/*
-		if (foundNormalBlindPickButton) {
-		clickLocation(normalBlindPickLocation.x + 5, normalBlindPickLocation.y + 5);
-		scanForNormalBlindPick = false;
-		currentStep = STEP_6;
-		actionClick = mach_absolute_time();
-		}*/
 		//clickLocation(playButtonLocation.x +300, playButtonLocation.y +180);
 	}break;
 	case STEP_6: {
@@ -415,51 +400,6 @@ bool AutoQueueManager::processDetection(ImageData data, const CGRect* rects, siz
 		return false;
 	}
 
-	/*
-	dispatch_group_t dispatchGroup = dispatch_group_create();
-
-
-	dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
-	dispatch_group_async(dispatchGroup, queue, ^{
-	float returnPercentage3 = 0.0;
-	Position returnPosition3;
-	int xStart2 = 0;
-	int yStart2 = 0;
-	int xEnd2 = data.imageWidth;
-	int yEnd2 = data.imageHeight;
-	CGRect search2 = CGRectMake(xStart2, yStart2, xEnd2 - xStart2, yEnd2-yStart2);
-	size_t intersectRectsNum2;
-	CGRect* intersectSearch2 = getIntersectionRectangles(search2, rects, num_rects, intersectRectsNum2);
-
-	detectExactImageToImageToRectangles(testImage1, data, intersectSearch2, intersectRectsNum2, returnPercentage3, returnPosition3, 0.9, true);
-
-	if (returnPercentage3 > 0.9) {
-	clickLocation(returnPosition3.x, returnPosition3.y);
-	}
-	});
-
-	queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
-	dispatch_group_async(dispatchGroup, queue, ^{
-	float returnPercentage3 = 0.0;
-	Position returnPosition3;
-	int xStart2 = 0;
-	int yStart2 = 0;
-	int xEnd2 = data.imageWidth;
-	int yEnd2 = data.imageHeight;
-	CGRect search2 = CGRectMake(xStart2, yStart2, xEnd2 - xStart2, yEnd2-yStart2);
-	size_t intersectRectsNum2;
-	CGRect* intersectSearch2 = getIntersectionRectangles(search2, rects, num_rects, intersectRectsNum2);
-
-	detectExactImageToImageToRectangles(testImage2, data, intersectSearch2, intersectRectsNum2, returnPercentage3, returnPosition3, 0.9, true);
-	////NSLog(@"Test image 1: %f", returnPercentage3);
-	if (returnPercentage3 > 0.9) {
-	clickLocation(returnPosition3.x, returnPosition3.y);
-	}
-	});
-	dispatch_group_wait(dispatchGroup, DISPATCH_TIME_FOREVER);
-	//To speed up the search we will use the CGRectIntersection function, cut down on the search area
-	//We will also make each detection run on a separate thread for extra speeds
-	*/
 
 
 	__block volatile bool fireLogic = false;
@@ -653,38 +593,6 @@ bool AutoQueueManager::processDetection(ImageData data, const CGRect* rects, siz
 		}
 
 		});
-		/*
-
-		dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
-		dispatch_group_async(dispatchGroup, queue, ^{
-
-		float returnPercentage = 0.0;
-		Position returnPosition;
-		int xStart = detectionPlayButtonReferenceLocation.x-400;
-		int yStart = detectionPlayButtonReferenceLocation.y;
-		int xEnd = detectionPlayButtonReferenceLocation.x+400;
-		int yEnd = detectionPlayButtonReferenceLocation.y+600;
-
-
-		CGRect search = CGRectMake(xStart, yStart, xEnd - xStart, yEnd-yStart);
-		size_t intersectRectsNum;
-		CGRect* intersectSearch = getIntersectionRectangles(search, rects, num_rects, intersectRectsNum);
-
-		detectExactImageToImageToRectangles(step8_RandomChampionButton, data, intersectSearch, intersectRectsNum, returnPercentage, returnPosition, 0.83, true);
-
-		if (returnPercentage >= 0.3) {
-		dispatch_async(dispatch_get_main_queue(), ^{
-		foundRandomChampionButton = true;
-		randomChampionButtonLocation = returnPosition;
-		});
-		fireLogic = true;
-		} else {
-		dispatch_async(dispatch_get_main_queue(), ^{
-		foundRandomChampionButton = false;
-		});
-		}
-
-		});*/
 	}
 	if (scanForLockInButton) {
 		foundLockInButton = false;
@@ -710,32 +618,7 @@ bool AutoQueueManager::processDetection(ImageData data, const CGRect* rects, siz
 				}
 			}
 		}
-		/*
-		float returnPercentage = 0.0;
-		Position returnPosition;
-		int xStart = detectionPlayButtonReferenceLocation.x-400;
-		int yStart = detectionPlayButtonReferenceLocation.y;
-		int xEnd = detectionPlayButtonReferenceLocation.x+500;
-		int yEnd = detectionPlayButtonReferenceLocation.y+600;
-
-
-		CGRect search = CGRectMake(xStart, yStart, xEnd - xStart, yEnd-yStart);
-		size_t intersectRectsNum;
-		CGRect* intersectSearch = getIntersectionRectangles(search, rects, num_rects, intersectRectsNum);
-
-		detectExactImageToImageToRectangles(step9_LockInButton, data, intersectSearch, intersectRectsNum, returnPercentage, returnPosition, 0.83, true);
-
-		if (returnPercentage >= 0.3) {
-		dispatch_async(dispatch_get_main_queue(), ^{
-		foundLockInButton = true;
-		lockInButtonLocation = returnPosition;
-		});
-		fireLogic = true;
-		} else {
-		dispatch_async(dispatch_get_main_queue(), ^{
-		foundLockInButton = false;
-		});
-		}*/
+		
 
 		});
 	}
@@ -847,71 +730,12 @@ bool AutoQueueManager::processDetection(ImageData data, const CGRect* rects, siz
 				}
 			}
 		}
-		/*
-		CGRect search = CGRectMake(xStart, yStart, xEnd - xStart, yEnd-yStart);
-		size_t intersectRectsNum;
-		CGRect* intersectSearch = getIntersectionRectangles(search, rects, num_rects, intersectRectsNum);
-		*/
 		//detectExactImageToImageToRectangles(step12_HomeButton, data, intersectSearch, intersectRectsNum, returnPercentage, returnPosition, 0.4, true);
 		////NSLog(@"Home button: %f", returnPercentage);
 		//free(intersectSearch);
-		/*
-		if (returnPercentage >= 0.4) {
-		dispatch_async(dispatch_get_main_queue(), ^{
-		//NSLog(@"Found home button at %d, %d", returnPosition.x, returnPosition.y);
-		foundHomeButton = true;
-		homeButtonLocation = returnPosition;
-		});
-		fireLogic = true;
-		} else {
-		dispatch_async(dispatch_get_main_queue(), ^{
-		foundHomeButton = false;
-		});
-		}
-		*/
 		});
 	}
 	if (scanForNormalBlindPick) {
-		/*
-		const int scanChunksX = 5; //36 frames until full scan. Full scan at 60fps is 0.6 seconds.
-		const int scanChunksY = 24;
-
-		float leagueGameWidth = data.imageWidth;
-		float leagueGameHeight = data.imageHeight;
-		CGRect leagueWindowRect = CGRectMake(0, 0, leagueGameWidth, leagueGameHeight);
-
-
-
-		int scanStartX = 0; int scanStartY = 0;
-		int scanEndX = leagueGameWidth; int scanEndY = leagueGameHeight;
-		int scanWidth = (scanEndX - scanStartX) / scanChunksX;
-		int scanHeight = (scanEndY - scanStartY) / scanChunksY;
-
-		int framesPassed = 1;//(getTimeInMilliseconds(mach_absolute_time() - processAllyChampionLastTime)) / 16;
-		//if (framesPassed <= 0) framesPassed = 1;
-		//if (framesPassed > allyChampionScanChunksX * allyChampionScanChunksY) framesPassed = allyChampionScanChunksX * allyChampionScanChunksY;
-
-		NSMutableArray* scanRectangles = [NSMutableArray new];
-		//Increase the scan chunk by 1
-		for (int i = 0; i < framesPassed; i++) {
-		step5ScanCurrentChunkX += 1;
-		if (step5ScanCurrentChunkX >= scanChunksX) {
-		step5ScanCurrentChunkX = 0;
-		step5ScanCurrentChunkY++;
-		}
-		if (step5ScanCurrentChunkY >= scanChunksY) {
-		step5ScanCurrentChunkY = 0;
-		}
-		//Add chunk to scan
-		CGRect scanRect = CGRectMake( scanWidth * step5ScanCurrentChunkX + scanStartX ,
-		scanHeight * step5ScanCurrentChunkY + scanStartY ,
-		scanWidth ,
-		scanHeight );
-		scanRect = CGRectIntegral(scanRect);
-		scanRect = fitRectangleInRectangle(scanRect, leagueWindowRect);
-		combineRectangles(scanRectangles, scanRect);
-		}
-		*/
 		dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
 		dispatch_group_async(dispatchGroup, queue, ^{
 
@@ -944,243 +768,5 @@ bool AutoQueueManager::processDetection(ImageData data, const CGRect* rects, siz
 	return fireLogic;
 
 
-	/*
-	ImageData imageData = data;
-	if ((clock() - lastScreenScan)/CLOCKS_PER_SEC >= 0.5) {
-	lastScreenScan = clock();
-	bool match = false;
-	Position clickLocation;
-	//location.x = 0; location.y = 0;
-	int xStart = 300;
-	int yStart = 0;
-	int xEnd = imageData.imageWidth - 400;
-	int yEnd = 250;
-	//NSLog(@"%d %d", imageData.imageWidth, imageData.imageHeight);
-	////NSLog(@"%d,%d to %d, %d", xStart, yStart, xEnd, yEnd);
-	float returnPercentage = 0.0;
-	Position returnPosition;
-
-	detectExactImageToImage(step1_PlayButton, imageData, xStart, yStart, xEnd, yEnd, returnPercentage, returnPosition, 0.83, true);
-
-	if (returnPercentage >= 0.3) {
-	//NSLog(@"Found play button");
-	playButtonLocation = returnPosition;
-	clickLocation = playButtonLocation;
-	match = true;
-	currentStep = STEP_1;
-	} else {
-	//NSLog(@"Did not find play button");
-	}
-
-	xStart = 0;
-	yStart = 0;
-	xEnd = imageData.imageWidth;
-	yEnd = imageData.imageHeight;
-
-	switch (currentStep) {
-	case STEP_1: {
-	if (match)  {
-	currentStep = STEP_2;
-	//NSLog(@"Clicked play button");
-	}
-	}break;
-	case STEP_2: {
-	//Use relative location from play button
-	clickLocation.x = playButtonLocation.x -230;
-	clickLocation.y = playButtonLocation.y +75;
-	match = true;
-	currentStep = STEP_3;
-	if (match) {
-	//NSLog(@"Clicked PVP mode");
-	} else {//NSLog(@"PVP not detected, switching to next");}
-
-	// match = detectRelativeImageInImage(step2_PVPMode, data, location, 0.65);
-	// location.x += 191; location.y += 76;
-
-	}break;
-	case STEP_3: {
-	clickLocation.x = playButtonLocation.x -70;
-	clickLocation.y = playButtonLocation.y +105;
-	match = true;
-	//match = detectRelativeImageInImage(step3_ClassicMode, data, location, 0.65);
-	//location.x += 352; location.y += 114;
-	currentStep = STEP_4;
-	if (match) {//NSLog(@"Clicked classic mode");} else {//NSLog(@"Classic not detected,switching to next");}
-	}break;
-	case STEP_4: {
-	clickLocation.x = playButtonLocation.x +100;
-	clickLocation.y = playButtonLocation.y +110;
-	match = true;
-	//match = detectRelativeImageInImage(step4_SummonersRiftMode, data, location, 0.7);
-	//location.x += 510; location.y += 116;
-	currentStep = STEP_5;
-	if (match) {//NSLog(@"Clicked Summoners Rift Mode");} else {//NSLog(@"Rift mode not detected, switching to next");}
-	}break;
-	case STEP_5: {
-	clickLocation.x = playButtonLocation.x +300;
-	clickLocation.y = playButtonLocation.y +180;
-	match = true;
-	//match = detectRelativeImageInImage(step5_BlindPickMode, data, location, 0.7);
-	//location.x += 675; location.y += 114;
-	currentStep = STEP_6;
-	if (match) {//NSLog(@"Clicked blind pick");} else {//NSLog(@"Blind pick not detected, switching to next");}
-	}break;
-	case STEP_6: {
-	clickLocation.x = playButtonLocation.x +100;
-	clickLocation.y = playButtonLocation.y +550;
-	match = true;
-	//match = detectRelativeImageInImage(step6_SoloButton, data, location, 0.85);
-	//location.x += 19; location.y += 76;
-	if (match) currentStep = STEP_7;
-	if (match) {//NSLog(@"Clicked solo button");}
-	}break;
-	case STEP_7: {
-	detectExactImageToImage(step7_AcceptButton, imageData, playButtonLocation.x-100, playButtonLocation.y+100, playButtonLocation.x+100, playButtonLocation.y+450, returnPercentage, returnPosition, 0.83, TRUE);
-	if (returnPercentage >= 0.3) {
-	match = true;
-	clickLocation = returnPosition;
-	}
-	if (match) currentStep = STEP_8;
-	if (match) {//NSLog(@"Clicked accept button");}
-	}break;
-	case STEP_8: {
-	xStart = playButtonLocation.x-400;
-	yStart = playButtonLocation.y;
-	xEnd = playButtonLocation.x+400;
-	yEnd = playButtonLocation.y+600;
-	detectExactImageToImage(step8_RandomChampionButton, imageData, xStart, yStart, xEnd, yEnd, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	clickLocation = returnPosition;
-	match = true;
-	}
-	if (match) {
-	currentStep = STEP_9;
-	if (match) {//NSLog(@"Clicked random champion");}
-	} else {
-	detectExactImageToImage(step7_AcceptButton, imageData, playButtonLocation.x-100, playButtonLocation.y+100, playButtonLocation.x+100, playButtonLocation.y+450, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	//Someone queue dodged
-	match = true;
-	clickLocation = returnPosition;
-	currentStep = STEP_8;
-	if (match) {//NSLog(@"Clicked accept button");}
-	}
-	}
-	}break;
-	case STEP_9: {
-	xStart = playButtonLocation.x-400;
-	yStart = playButtonLocation.y;
-	xEnd = playButtonLocation.x+500;
-	yEnd = playButtonLocation.y+600;
-	detectExactImageToImage(step9_LockInButton, imageData, xStart, yStart, xEnd, yEnd, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	clickLocation = returnPosition;
-	match = true;
-	}
-	if (match) {
-	currentStep = STEP_10;
-	if (match) {//NSLog(@"Clicked lock in button");}
-	} else {
-	detectExactImageToImage(step7_AcceptButton, imageData, playButtonLocation.x-100, playButtonLocation.y+100, playButtonLocation.x+100, playButtonLocation.y+450, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	//Someone queue dodged
-	match = true;
-	clickLocation = returnPosition;
-	currentStep = STEP_8;
-	if (match) {//NSLog(@"Clicked accept button");}
-	}
-	}
-	}break;
-	case STEP_10: {
-	xStart = playButtonLocation.x-400;
-	yStart = playButtonLocation.y;
-	xEnd = playButtonLocation.x+500;
-	yEnd = playButtonLocation.y+600;
-	detectExactImageToImage(step10_ChooseSkinButton, imageData, xStart, yStart, xEnd, yEnd, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	returnPosition.x += 5; returnPosition.y += 156;
-	clickLocation = returnPosition;
-	match = true;
-	}
-	if (match) {//NSLog(@"Clicked skin change button");}
-	if (!match) currentStep = STEP_12;
-	//Check for accept button
-	detectExactImageToImage(step7_AcceptButton, imageData, playButtonLocation.x-100, playButtonLocation.y+100, playButtonLocation.x+100, playButtonLocation.y+450, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	//Someone queue dodged
-	match = true;
-	clickLocation = returnPosition;
-	currentStep = STEP_8;
-	if (match) {//NSLog(@"Clicked accept button");}
-	} else {
-	//Check for reconnect button
-	detectExactImageToImage(step13_ReconnectButton, imageData, playButtonLocation.x-100, playButtonLocation.y+100, playButtonLocation.x+200, playButtonLocation.y+450, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	match = true;
-	clickLocation = returnPosition;
-	//Reconnecting screen
-	if (match) {//NSLog(@"Clicked reconnect button");}
-	}
-	}
-	}break;
-	case STEP_12: {
-	xStart = playButtonLocation.x;
-	yStart = playButtonLocation.y+450;
-	xEnd = playButtonLocation.x+500;
-	yEnd = playButtonLocation.y+625;
-	if (playButtonLocation.x == -1) {
-	xStart = 0;
-	yStart = 0;
-	xEnd = imageData.imageWidth;
-	yEnd = imageData.imageHeight;
-	}
-	detectExactImageToImage(step12_HomeButton, imageData, xStart, yStart, xEnd, yEnd, returnPercentage, returnPosition, 0.83, true);
-	if (returnPercentage >= 0.3) {
-	clickLocation = returnPosition;
-	match = true;
-	}
-	if (match) currentStep = STEP_1;
-	if (match) {//NSLog(@"Clicked home button");}
-	}break;
-	}
-	if (match) {
-	//NSLog(@"Clicked location %d %d", clickLocation.x + 10, clickLocation.y+10);
-	floatTapMouseLeft(clickLocation.x + 10, clickLocation.y+10);
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC / 60.0), dispatch_get_main_queue(), ^{ // one
-	floatTapMouseLeft(clickLocation.x + 10, clickLocation.y+10);
-	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC / 5.0), dispatch_get_main_queue(), ^{ // one
-	moveMouse(0, 0);
-	});
-	//tapMouseLeft(location.x + 10, location.y+10);
-	}
-	}
-	*/
 }
-/*
-void AutoQueueManager::checkForEndGame(ImageData data) {
-imageData = data;
-
-if ((clock() - lastEndGameScan)/CLOCKS_PER_SEC >= 1.0) {
-//NSLog(@"Checking for end game button");
-lastEndGameScan = clock();
-
-int xStart = imageData.imageWidth/2 - 150;
-int yStart = imageData.imageHeight * 0.68125 - 100;
-int xEnd = imageData.imageWidth/2 + 150;
-int yEnd = imageData.imageHeight * 0.68125 + 100;
-float returnPercentage;
-Position returnPosition;
-detectExactImageToImage(step11_EndGameContinueButton, imageData, xStart, yStart, xEnd, yEnd, returnPercentage, returnPosition, 0.65, true);
-if (returnPercentage >= 0.65) {
-floatTapMouseLeft(returnPosition.x + 5, returnPosition.y+5);
-moveMouse(returnPosition.x + 5, returnPosition.y+5);
-dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC / 60.0), dispatch_get_main_queue(), ^{ // one
-floatTapMouseLeft(returnPosition.x + 5, returnPosition.y+5);
-});
-//NSLog(@"Clicked end game button");
-}
-currentStep = STEP_12;
-}
-}*/
 */
