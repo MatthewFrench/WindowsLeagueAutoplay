500159631b19bf01a601a174f2e817666309ba37
MatthewFrench <matthewf@ksu.edu>
2016-09-09 19:38:13 -0500

    Tiny bit less verbose

--- League Autoplay/League Autoplay/BasicAI.cs
+++ League Autoplay/League Autoplay/BasicAI.cs
@@ -88,7 +88,7 @@ namespace League_Autoplay
         {
             if (newData)
             {
-                Console.WriteLine("Processing AI");
+                Console.Write("{P}");
             }
             else
             {
